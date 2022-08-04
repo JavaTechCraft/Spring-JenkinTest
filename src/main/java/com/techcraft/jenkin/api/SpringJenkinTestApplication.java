807547e80ed93086fc6f0d78190b8b485b7c6236
Java Techcraft <java528.techcraft@gmail.com>
2022-08-04 14:23:35 +0100

    second commit

--- src/main/java/com/techcraft/jenkin/api/SpringJenkinTestApplication.java
+++ src/main/java/com/techcraft/jenkin/api/SpringJenkinTestApplication.java
@@ -12,6 +12,27 @@ public class SpringJenkinTestApplication {
 
 	public void init() {
 		logger.info("Application is started...");
+		
+		/* 
+			 	or create a new repository on the command line
+				echo "# Spring-JenkinTest" >> README.md
+				git init
+				git add README.md
+				git commit -m "first commit"
+				git branch -M main
+				git remote add origin https://github.com/JavaTechCraft/Spring-JenkinTest.git
+				git push -u origin main
+				
+				…or push an existing repository from the command line
+				
+				git remote add origin https://github.com/JavaTechCraft/Spring-JenkinTest.git
+				git branch -M main
+				git push -u origin main  
+		 * */
+		
+		
+		
+		
 	}
 
 	public static void main(String[] args) {
